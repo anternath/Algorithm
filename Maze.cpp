63d29822abc4eb1cbd078bc57a791f322166a0c2
Anter Nath <anternath73@gmail.com>
2025-07-06 16:09:03 +0600

    dfs bfs dsu mst related problem solved in hackerrank

--- Maze.cpp
+++ Maze.cpp
@@ -9,11 +9,12 @@ bool valid(int i, int j){
     }
     else return true;
 }
+pair<int,int> des[1005][1005];
 void bfs(int si,int sj){
     queue<pair<int,int>> q;
     vis[si][sj]=true;
     q.push({si,sj});
-    pair<int,int> pth[4]={{1,0},{-1,0},{0,1},{0,-1}};
+    pair<int,int> pth[4]={{0,1},{0,-1},{-1,0},{1,0}};
     while(!q.empty()){
         pair<int,int> par= q.front();
         q.pop();
@@ -24,11 +25,13 @@ void bfs(int si,int sj){
             int cj = pth[i].second+sj;
             if(valid(ci,cj)==true && vis[ci][cj]==false &&(ar[ci][cj]=='.'|| ar[ci][cj]=='D')){
                 if(ar[ci][cj]=='D'){
+                    vis[ci][cj]=true;
+                    des[ci][cj]={si,sj};
                     return;
                 }
                 else{
                     q.push({ci,cj});
-                    ar[ci][cj]='X';
+                    des[ci][cj]={si,sj};
                     vis[ci][cj]=true;
                 }
             }
@@ -42,18 +45,39 @@ int main(){
             cin>>ar[i][j];
         }
     }
+    int di,dj;
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             if(ar[i][j]=='R'){
                 bfs(i,j);
             }
+            else if(ar[i][j]=='D'){
+                di=i;
+                dj=j;
+            }
         }
     }
-    for(int i=0; i<n; i++){
+    if(vis[di][dj]){
+        pair<int,int> cur={di,dj};
+        while(ar[cur.first][cur.second]!='R'){
+            cur= des[cur.first][cur.second];
+            if(ar[cur.first][cur.second]=='R') break;
+            ar[cur.first][cur.second]='X';
+        }
+        for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-           cout<<ar[i][j];
+            cout<<ar[i][j];
         }
         cout<<endl;
+    }
+    }
+    else{
+        for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<ar[i][j];
+        }
+        cout<<endl;
+    }
     }
     return 0;
 }
